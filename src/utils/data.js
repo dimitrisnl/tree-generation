a722e8370880b10b133ae354f581283bf9abe36b
Dimitrios Lytras <dnlytras@gmail.com>
2018-03-03 17:43:57 +0200

    update tree generation aglorithm

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,11 +2,21 @@ const fallbackText =
   '\
 page 1\n\
   page 1.1\n\
+    page 1.1.1\n\
+    page 1.1.2\n\
+      page 1.1.2.1\n\
   page 1.2\n\
     page 1.2.1\n\
+  page 1.3\n\
+    page 1.3.1\n\
+    page 1.3.2\n\
+      page 1.3.2.1\n\
+  page 1.4\n\
+  page 1.5\n\
 page 2\n\
 page 3\n\
   page 3.1\n\
+    page 3.1.1\n\
   page 3.2\n\
 page 4\n\
   page 4.1\n\
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,20 +1,35 @@
 export const prepareTree = (text = '') => {
   const root = { title: null, isRoot: true, children: [] };
-  let previousNode = null;
+  let parentNode = root;
+  let parentNodeHistory = [root];
 
   const arr = convertToArray(text);
 
   arr.forEach((node, index) => {
-    const newObj = { title: node.value, isRoot: false, children: [] };
+    const currentNode = { title: node.value, isRoot: false, children: [] };
+    const nextNode = arr[index + 1];
 
-    const parentNode = node.identation === 0 ? root : previousNode;
-    parentNode.children.push(newObj);
+    parentNode.children.push(currentNode);
 
-    if (arr[index + 1] && arr[index + 1].identation > node.identation) {
-      previousNode = newObj;
+    if (nextNode) {
+      if (nextNode.indentation === 0) {
+        parentNode = root;
+        parentNodeHistory = [root];
+        // next node should be nested
+      } else if (nextNode.indentation > node.indentation) {
+        parentNode = currentNode;
+        parentNodeHistory.unshift(parentNode);
+      } else {
+        // next node belongs to upper level, go back in chain to find the parent
+        for (let i = 0; i < node.indentation - nextNode.indentation; i++) {
+          parentNodeHistory.shift();
+        }
+        parentNode = parentNodeHistory[0];
+      }
     }
   });
 
+  parentNodeHistory = null;
   return root;
 };
 
@@ -24,14 +39,14 @@ const convertToArray = string => {
     string = string.replace('\n\n', '\n');
   }
 
-  /*
-    Remove initial space, which would result in the first element
-    being treated as a 'child' of another node
-  */
+  //  Trim initial space to avoid any bamboozles
   string = string.trimLeft();
 
-  // Split by new line character and keep the identation count
+  // Split by new line character and keep the indentation count
   return string.split('\n').map(node => {
-    return { value: node.trim(), identation: node.length - node.trim().length };
+    return {
+      value: node.trim(),
+      indentation: (node.length - node.trimLeft().length) / 2,
+    };
   });
 };
